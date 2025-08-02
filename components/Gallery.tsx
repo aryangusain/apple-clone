4a7a29fc71f90bee5c633e09f4d0b7a19ccfa566
aryangusain <aryangusain123@gmail.com>
2025-08-02 13:20:12 +0530

    complete desktop version

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,16 +1,36 @@
 import GalleryCard from "./ui/GalleryCard";
 
 const Gallery = () => {
-  const cards = ["/gallery_card_1.jpg", "/gallery_card_2.jpg", "gallery_card_3.jpg", "gallery_card_4.jpg", "gallery_card_6.jpg"];
+  const cards = [
+    {url: "/gallery_card_1.png", text: "Watch Now" },
+    {url: "/gallery_card_2.png", text: "Listen Now" },
+    {url: "/gallery_card_3.png", text: "Play Now" },
+    {url: "/gallery_card_4.png", text: "Watch Now" },
+    {url: "/gallery_card_5.png", text: "Listen Now"},
+    {url: "/gallery_card_6.png", text: "Play Now" },
+    {url: "/gallery_card_1.png", text: "Watch Now" },
+    {url: "/gallery_card_2.png", text: "Listen Now" },
+    {url: "/gallery_card_3.png", text: "Play Now" },
+    {url: "/gallery_card_4.png", text: "Watch Now" },
+    {url: "/gallery_card_5.png", text: "Listen Now"},
+    {url: "/gallery_card_6.png", text: "Play Now" },
+    {url: "/gallery_card_1.png", text: "Watch Now" },
+    {url: "/gallery_card_2.png", text: "Listen Now" },
+    {url: "/gallery_card_3.png", text: "Play Now" },
+    {url: "/gallery_card_4.png", text: "Watch Now" },
+    {url: "/gallery_card_5.png", text: "Listen Now"},
+    {url: "/gallery_card_6.png", text: "Play Now" },
+  ];
 
   return (
-    <div className="w-full flex">
-        {
-          cards.map((url, index) => {
-            return <GalleryCard key={index} url={url} />
-          })
-        }
+    <div className="w-full overflow-hidden my-[40px]">
+      <div className="flex w-fit gap-[16px] animate-scroll">
+        {cards.map((card, index) => {
+          return <GalleryCard key={index} text={card.text} url={card.url} />;
+        })}
+      </div>
     </div>
-  )
-}
-export default Gallery
+  );
+};
+
+export default Gallery;
--- components/ui/GalleryCard.tsx
+++ components/ui/GalleryCard.tsx
@@ -1,11 +1,12 @@
-import { cn } from "@/lib/utils"
+import Button from "./Button"
 
-const GalleryCard = ({url}: {url: string}) => {
+const GalleryCard = ({url, text}: {url: string, text: string}) => {
   return (
-    <div className={cn(
-        `bg-[url(${url})] h-full bg-cover bg-no-repeat bg-center`
-    )}>
-        
+    <div className="h-[230px] min-w-[417px] cursor-pointer group relative">
+        <img src={url} className="h-full w-full -z-10 absolute"/>
+        <div className="z-10 opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-hover:bg-black/30 flex h-full w-full justify-center items-center">
+            <Button className="z-10 bg-white border-white text-black hover:bg-text-secondary w-fit text-sm" variant="primary">{text}</Button>
+        </div>
     </div>
   )
 }
